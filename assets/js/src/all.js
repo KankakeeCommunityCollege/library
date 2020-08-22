fc85c214df57a50d8142b76159287ea451caf125
wdzajicek <tig.welder4@gmail.com>
2020-08-21 20:49:03 -0500

    Hack DataTables plugin into iframe-widget.

--- assets/js/src/all.js
+++ assets/js/src/all.js
@@ -1,8 +1,10 @@
 import setIframeHeight from './setIframeHeight.js';
 import styleIframe from './styleIframe.js';
+//import makeAccordionLinkable from './makeAccordionLinkable.js';
 //import './babelTest.js';
 
 document.addEventListener('DOMContentLoaded', function() {  
   setIframeHeight();
-  //styleIframe();
+  styleIframe();
+  //makeAccordionLinkable();
 });
--- assets/js/src/styleIframe.js
+++ assets/js/src/styleIframe.js
@@ -12,20 +12,55 @@ table {
   return style;
 }
 
+function createDataTablesScript() {
+  const script = document.createElement('script');
+  
+  script.type = 'text/javascript';
+  const code = `
+  function go() {
+    return $('#Data').DataTable();
+  });
+  go();
+  `;
+  const scriptCode = document.createTextNode(code);
+  return script.appendChild(scriptCode);
+}
+
 function styleIframe() {
   if ( ! document.querySelector('.iframeTableStyling') )
     return;
 
   const iframe = document.querySelector('.iframeTableStyling');
-  const style = createStyleEl();
+  //const style = createStyleEl();
 
 
   console.log(iframe);
   iframe.addEventListener('load', ()=> {
-    console.log(iframe.contentDocument.head);
+    console.log(iframe.contentDocument.body.querySelector('table.table'));
+    const table = iframe.contentDocument.body.querySelector('table.table');
+    const script = createDataTablesScript();
+    
+    table.classList.add('table', 'table-striped', 'responsive');
+    table.id = 'Data';
+    const list = table.querySelectorAll('th');
+    console.log(list);
 
-    const iframeHead = iframe.contentDocument.head;
-    iframeHead.appendChild(style);
+    for (let i = 0, len = list.length; i < len; i++ ) {
+      const th = list[i];
+      
+      i === 0 || i === 1 ? th.classList.add('all') : th.classList.add('none');
+    }
+    
+    $(table).DataTable({
+      paging: false,
+      info: false//,
+      //responsive: true
+    });
+
+    const $table = $(table)
+    new $.fn.dataTable.Responsive($table, {  // For some reason 'Responsive' extension won't initiate on initial DataTable() call. Only works on iframe after DataTables() has already been initiated
+      info: false
+    });
   });
 }
 
